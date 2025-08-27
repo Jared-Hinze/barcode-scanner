f0a90241ec274f4f4c9afa6650ba1df895d86c3b
Jared Hinze <Jared.L.Hinze@gmail.com>
2025-08-26 22:57:17 -0500

    Jhinze/generate tickets and sheets (#2)
    Create a quick layout of basic ideas and file structure for generating barcodes on tickets and sheets of tickets and tying them to the database.

---
+++ src/paths.py
@@ -0,0 +1,48 @@
+# Built-in Libraries
+import sys
+from pathlib import Path
+
+# Third Party Libraries
+# N/A
+
+# Local Libraries
+# N/A
+
+
+# ==============================================================================
+# Helpers
+# ==============================================================================
+def ensure_directory(*paths):
+	p = Path(*paths)
+	if not p.is_dir():
+		p.mkdir(exist_ok=True, parents=True)
+	return p
+
+
+# ------------------------------------------------------------------------------
+def ensure_file(base_dir, file):
+	p = ensure_directory(base_dir)
+	p /= file
+	if not p.is_file():
+		p.touch(exist_ok=True)
+	return p
+
+
+# ==============================================================================
+# Paths
+# ==============================================================================
+if getattr(sys, "frozen", False):
+	BASEDIR = Path(sys.executable).parent
+	DIR_ASSETS = Path(sys._MEIPASS) / "assets"
+else:
+	BASEDIR = Path(__file__).parent
+	DIR_ASSETS = BASEDIR / "assets"
+
+# Assets
+DIR_IMAGES = ensure_directory(DIR_ASSETS, "images")
+DIR_SOUNDS = ensure_directory(DIR_ASSETS, "sounds")
+
+# Outputs
+DIR_OUTPUTS = BASEDIR / "outputs"
+DIR_SHEETS = ensure_directory(DIR_OUTPUTS / "sheets")
+DIR_TICKETS = ensure_directory(DIR_OUTPUTS / "tickets")
